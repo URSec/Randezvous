5007d9c3fd600ba8dad9f38af3aee66e8ced6316
Zhuojia Shen <chaosdefinition@hotmail.com>
2021-03-25 13:59:09 -0700

    Add a script to compile and run mbedTLS Benchmark.
    Add a script that compiles and runs mbedTLS Benchmark and update
    import.sh to be able to import the project into the IDE.

---
+++ scripts/mbedtls-benchmark.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+ROOT_DIR=`dirname $0 | sed 's/$/\/../' | xargs realpath`
+
+PROJ=mbedtls-benchmark
+PROJ_DIR="$ROOT_DIR/workspace/$PROJ"
+
+PROGRAMS=(
+    "mbedtls-benchmark"
+)
+
+CONFIGURATIONS=(
+    "baseline"
+)
+
+#
+# Load common components.
+#
+. "$ROOT_DIR/scripts/common.sh"
+
+#
+# Entrance of the script.
+#
+case $1 in
+"debug" )
+    debug $2 $3
+    ;;
+"run" )
+    if (( $# == 2 )); then
+        for program in ${PROGRAMS[@]}; do
+            run $2 $program "Bye!"
+        done
+    else
+        run $2 $3 "Bye!"
+    fi
+    ;;
+* )
+    if (( $# == 1 )); then
+        # Compile each benchmark program
+        for program in ${PROGRAMS[@]}; do
+            compile $1 $program
+        done
+
+        echo Done
+    else
+        compile $1 $2
+    fi
+    ;;
+esac
